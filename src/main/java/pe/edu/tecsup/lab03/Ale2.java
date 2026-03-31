8d85f53b9540e3e8533ce140fd994e1039ea435d
nadhiraatanacio-oss <nadhira.atanacio@tecsup.edu.pe>
2026-03-31 17:06:57 -0500

    666

--- src/main/java/pe/edu/tecsup/lab03/Ale2.java
+++ src/main/java/pe/edu/tecsup/lab03/Ale2.java
@@ -1,10 +1,10 @@
 package pe.edu.tecsup.lab03;
 
-public class Application {
+public class Ale2 {
 
     public static void main(String[] args) {
         // TO DO
-
+        int Alejandra = 666;
     }
 
 }
